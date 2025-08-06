3c4db72934fac0ac7cb4871de6541260f9f1a216
Aromal M S <aromalms3318@gmail.com>
2025-08-06 23:36:00 +0530

    made some changes

--- main.js
+++ main.js
@@ -7,10 +7,37 @@ function openTab(tabName) {
     document.getElementById(tabName).classList.add('active-tab');
 }
 
-var sidemeu=document.getElementById("sidemenu");
-function openmenu(){
-    sidemeu.style.right="0";
+// Mobile sidemenu toggle
+const sidemenu = document.getElementById("sidemenu");
+const barIcon = document.getElementById("bar-icon");
+const closeIcon = document.getElementById("close-icon");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+    barIcon.classList.add("hide");
+    closeIcon.style.display = "block";
 }
-function closemenu(){
-    sidemeu.style.right="-200px";
+
+function closemenu() {
+    sidemenu.style.right = "-250px";
+    barIcon.classList.remove("hide");
+    closeIcon.style.display = "none";
 }
+
+// Hide close icon by default on load
+closeIcon.style.display = "none";
+barIcon.classList.remove("hide");
+
+// Update resize handler as well
+window.addEventListener('resize', function() {
+    if(window.innerWidth > 600){
+        barIcon.classList.add("hide");
+        closeIcon.style.display = "none";
+        sidemenu.style.right = "0";
+    } else {
+        barIcon.classList.remove("hide");
+        closeIcon.style.display = "none";
+        sidemenu.style.right = "-250px";
+    }
+});
+
